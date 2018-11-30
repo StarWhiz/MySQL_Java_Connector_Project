54daca3bd678b08e8868901ffa4260a776690697
Tai <taidao90@gmail.com>
2018-11-30 15:37:30 -0800

    Refactored: Created 17 functions for 17 requirements.

--- OnlineStore/src/App.java
+++ OnlineStore/src/App.java
@@ -41,9 +41,9 @@ public class App {
     }
 
     /**
-     * This function prints the Menu...
+     * This function prints the Main Menu...
      */
-    public static void printMainMenu(Statement stmt) throws SQLException {
+    private static void printMainMenu(Statement stmt) throws SQLException {
         boolean exitRequest = false; // for the Main Menu
         Scanner in = new Scanner(System.in);
 
@@ -52,7 +52,7 @@ public class App {
                     + "Are you a customer or a business owner?\n"
                     + "    1. Customer\n"
                     + "    2. Business owner\n"
-                    + "    3. Exit Store...");
+                    + "    3. Exit Store ...");
 
             int userLevel = in.nextInt();
 
@@ -67,10 +67,12 @@ public class App {
         }
     }
 
-    public static void printCustomerMenu(Statement stmt) throws SQLException {
+    /**
+     * This function prints the Customer Menu...
+     */
+    private static void printCustomerMenu(Statement stmt) throws SQLException {
         boolean exitRequested = false; // for the Customer Menu
         Scanner in = new Scanner(System.in);
-        ResultSet rs;
 
         while (!exitRequested) {
             System.out.println("You are a customer.\n");
@@ -80,21 +82,10 @@ public class App {
                     + "   3. \n"  // TODO
                     + "   4. Logout (customer logout)\n");
             int customerOption = in.nextInt();
+
             switch (customerOption) {
                 case 1:
-                    rs = stmt.executeQuery("SELECT itemname, \n" +
-                            "       Avg(rating) AS avgRating \n" +
-                            "FROM   items, \n" +
-                            "       reviews \n" +
-                            "WHERE  items.itemid = reviews.itemid \n" +
-                            "GROUP  BY items.itemid \n" +
-                            "ORDER  BY avgrating DESC \n" +
-                            "LIMIT  5;");
-                    System.out.format("%-15s\n", "Items");
-                    while (rs.next()) {
-                        System.out.format("%-15s\n", rs.getString("itemname"));
-                    }
-                    System.out.println("\n\n");
+                    functionRequirement10(stmt);
                     break;
                 case 2:
                     //TODO
@@ -109,10 +100,12 @@ public class App {
         }
     }
 
-    public static void printBusinessOwnerMenu(Statement stmt) throws SQLException{
+    /**
+     * This function prints the Business Owner Menu...
+     */
+    private static void printBusinessOwnerMenu(Statement stmt) throws SQLException{
         boolean exitRequested = false; // for the Customer Menu
         Scanner in = new Scanner(System.in);
-        ResultSet rs;
 
         while (!exitRequested) {
             System.out.println("You are a business owner.\n");
@@ -121,56 +114,206 @@ public class App {
                     + "   2. Show the total revenue of high-value items sold (items that are priced " +
                     "over $50)\n" // #2
                     + "   3. Show how much $ was lost due per items\n" // #4
+                    //TODO Add more menu items
                     + "   4. Exit\n");
             int besinessOwnerOption = in.nextInt();
+
             switch (besinessOwnerOption) {
                 case 1 :
-                    rs = stmt.executeQuery("SELECT C.customerid,\n" +
-                            "       C.customername,\n" +
-                            "       Sum(itemprice) AS totalPrice\n" +
-                            "FROM   customers AS C,\n" +
-                            "       billofsale AS B\n" +
-                            "WHERE  C.customerid = B.customerid\n" +
-                            "GROUP  BY C.customerid\n" +
-                            "ORDER  BY totalprice DESC\n" +
-                            "LIMIT  5;");
-                    System.out.format("%-15s%-30s%-15s\n", "Customer ID", "Customer name", "Total spent");
-                    while(rs.next()) {
-                        System.out.format("%-15s%-30s%-15s\n", rs.getInt("customerid"),
-                                rs.getString("customername"),
-                                rs.getString("totalPrice"));
-                    }
-                    System.out.println("\n\n");
+                    functionRequirement1(stmt);
                     break;
                 case 2:
-                    rs = stmt.executeQuery("SELECT Sum(I.itemprice) AS totalRevenue\n" +
-                            "FROM   items AS I\n" +
-                            "WHERE  I.itemprice > 50;");
-                    while(rs.next()) {
-                        System.out.println("Total revenue = " + rs.getInt("totalRevenue"));
-                    }
-                    System.out.println("\n\n");
+                    functionRequirement2(stmt);
                     break;
                 case 3:
-                    rs = stmt.executeQuery("SELECT B.itemname,\n" +
-                            "       Sum(B.itemprice * IT.qtymissing) AS TotalLoss\n" +
-                            "FROM   itemgraveyard AS IT,\n" +
-                            "       items AS B\n" +
-                            "WHERE  IT.itemid = B.itemid\n" +
-                            "       AND IT.qtymissing > 0\n" +
-                            "GROUP  BY( B.itemid );");
-                    System.out.format("%-30s%-15s\n", "Item name", "Total loss");
-                    while(rs.next()) {
-                        System.out.format("%-30s%-15s\n", rs.getString("itemname"),
-                                rs.getString("TotalLoss"));
-                    }
-                    System.out.println("\n\n");
+                    functionRequirement4(stmt);
                     break;
                 case 4:
                     exitRequested = true;
                     break;
             }
         }
+    }
+
+    /**
+     * Functional Requirement 1: As a business manager, I want to be able to see who my top 5 customer are. (People that spends the most).
+     */
+    private static void functionRequirement1(Statement stmt) throws SQLException {
+        ResultSet rs;
+        rs = stmt.executeQuery("SELECT C.customerid,\n" +
+                "       C.customername,\n" +
+                "       Sum(itemprice) AS totalPrice\n" +
+                "FROM   customers AS C,\n" +
+                "       billofsale AS B\n" +
+                "WHERE  C.customerid = B.customerid\n" +
+                "GROUP  BY C.customerid\n" +
+                "ORDER  BY totalprice DESC\n" +
+                "LIMIT  5;");
+        System.out.format("%-15s%-30s%-15s\n", "Customer ID", "Customer name", "Total spent");
+        while(rs.next()) {
+            System.out.format("%-15s%-30s%-15s\n", rs.getInt("customerid"),
+                    rs.getString("customername"),
+                    rs.getString("totalPrice"));
+        }
+        System.out.println();
+    }
+
+    /**
+     * Functional Requirement 2: As a business manager, I should be able to see the total revenue of all my high-value items sold (items that are priced over $50)
+     */
+    private static void functionRequirement2(Statement stmt) throws SQLException {
+        ResultSet rs;
+        rs = stmt.executeQuery("SELECT Sum(I.itemprice) AS totalRevenue\n" +
+                "FROM   items AS I\n" +
+                "WHERE  I.itemprice > 50;");
+        while(rs.next()) {
+            System.out.println("Total revenue = " + rs.getInt("totalRevenue"));
+        }
+        System.out.println();
+    }
+
+    /**
+     * TODO
+     * Functional Requirement 3: As a business manager, I should be able to see which 5 items were the worst sellers (lowest qty sold)
+     */
+    public static void functionRequirement3(Statement stmt) throws SQLException {
+        ResultSet rs;
+    }
+
+    /**
+     * Functional Requirement 4: As a business manager, I should be able to see how much $ was lost per items.
+     */
+    private static void functionRequirement4(Statement stmt) throws SQLException {
+        ResultSet rs;
+        rs = stmt.executeQuery("SELECT B.itemname,\n" +
+                "       Sum(B.itemprice * IT.qtymissing) AS TotalLoss\n" +
+                "FROM   itemgraveyard AS IT,\n" +
+                "       items AS B\n" +
+                "WHERE  IT.itemid = B.itemid\n" +
+                "       AND IT.qtymissing > 0\n" +
+                "GROUP  BY( B.itemid );");
+        System.out.format("%-30s%-15s\n", "Item name", "Total loss");
+        while(rs.next()) {
+            System.out.format("%-30s%-15s\n", rs.getString("itemname"),
+                    rs.getString("TotalLoss"));
+        }
+        System.out.println();
+    }
+
+    /**
+     * TODO
+     * Functional Requirement 5: As a business manager, I should be able to see the total number of items that are missing?
+     */
+    private static void functionRequirement5(Statement stmt) throws SQLException {
+        ResultSet rs;
+    }
+
+    /**
+     * TODO
+     * Functional Requirement 6: As a business manager, I want to see who my top 5 suppliers are (supplied the most items)
+     */
+    private static void functionRequirement6(Statement stmt) throws SQLException {
+        ResultSet rs;
+    }
+
+    /**
+     * TODO
+     * Functional Requirement 7: As a business manager, I should be able to see which customer has the highest return rates
+     */
+    private static void functionRequirement7(Statement stmt) throws SQLException {
+        ResultSet rs;
+    }
+
+    /**
+     * TODO
+     * Functional Requirement 8: As a business manager, I should be able to see which customers have the lowest return rates.
+     */
+    private static void functionRequirement8(Statement stmt) throws SQLException {
+        ResultSet rs;
+    }
+
+    /**
+     * TODO
+     * Functional Requirement 9: As a business manager and a customer, I should be able to see which item is the most expensive
+     */
+    private static void functionRequirement9(Statement stmt) throws SQLException {
+        ResultSet rs;
+    }
+
+    /**
+     * Functional Requirement 10: As a customer, I want to see the top 5 rated items.
+     */
+    private static void functionRequirement10(Statement stmt) throws SQLException {
+        ResultSet rs;
+        rs = stmt.executeQuery("SELECT itemname, \n" +
+                "       Avg(rating) AS avgRating \n" +
+                "FROM   items, \n" +
+                "       reviews \n" +
+                "WHERE  items.itemid = reviews.itemid \n" +
+                "GROUP  BY items.itemid \n" +
+                "ORDER  BY avgrating DESC \n" +
+                "LIMIT  5;");
+        System.out.format("%-15s\n", "Items");
+        while (rs.next()) {
+            System.out.format("%-15s\n", rs.getString("itemname"));
+        }
+        System.out.println();
+    }
+
+    /**
+     * TODO
+     * Functional Requirement 11: As a customer, I should be able to view all available items (1 or more qtyinStock) and all unavailable items (0 qtyinStock) separately
+     */
+    private static void functionRequirement11(Statement stmt) throws SQLException {
+        ResultSet rs;
+    }
+
+    /**
+     * TODO
+     * Functional Requirement 12: As a business manager I want to know who is my first customer?
+     */
+    private static void functionRequirement12(Statement stmt) throws SQLException {
+        ResultSet rs;
+    }
+
+    /**
+     * TODO
+     * Functional Requirement 13: As a business manager, I see the top 5 items that I have the most in stock.
+     */
+    private static void functionRequirement13(Statement stmt) throws SQLException {
+        ResultSet rs;
+    }
+
+    /**
+     * TODO
+     * Functional Requirement 14: As a customer, I want to know which items are top 5 best-selling (sold the most).
+     */
+    private static void functionRequirement14(Statement stmt) throws SQLException {
+        ResultSet rs;
+    }
+
+    /**
+     * TODO
+     * Functional Requirement 15: As a business manager, I should be able to see which supplier provides me with the highest quantity ordered.
+     */
+    private static void functionRequirement15(Statement stmt) throws SQLException {
+        ResultSet rs;
+    }
+
+    /**
+     * TODO
+     * Functional Requirement 16: As a business manager, I should be able to see the lowest quantity that a supplier offers.
+     */
+    private static void functionRequirement16(Statement stmt) throws SQLException {
+        ResultSet rs;
+    }
+
+    /**
+     * TODO
+     * Functional Requirement 17: As a customer, I should be able to view my purchase history.
+     */
+    private static void functionRequirement17(Statement stmt) throws SQLException {
+        ResultSet rs;
     }
 }
 
